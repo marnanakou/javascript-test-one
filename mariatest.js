5378f24102921768b6a976b614d0ccea751bd5c3
DCI <you@example.com>
2019-07-16 11:53:05 +0200

    here is the test...

---
+++ mariatest.js
@@ -0,0 +1,60 @@
+// 3.Given two strings, firstName and lastName, return a single string in the format "last, first".
+let firstName = "Maria";
+let lastName = "Nanakou";
+console.log(firstName,lastName);
+
+//5.Write a program that adds a string ending to each item in an array.
+
+let fruits = ['apple', 'banana','peach','kiwi'];
+console.log(fruits.push('melon'));
+console.log(fruits);
+
+//10.Write a program to multiply each value in an array by the number of items in that array.
+let startNum = 1;
+let endNumber = 50;
+function multiplyNumbers(startNumber,endNumber){
+    for (let i = 1; i <= 50 i++) {
+       return (num * i)
+    }
+}
+
+//9.Write a program that takes a string and returns the word count. The string will be a sentence.
+let word = "count";
+console.log(`The total score + ${'word'} + for the match`);
+
+//8.Make a variable with the string value of "maria jane jones". Convert each first letter to uppercase. Make sure your code works for any three names e.g. "john james smith".
+let name = "maria jane jones";
+console.log(toUpperCase());
+
+//6.Write a program that checks whether a number is even or odd and returns "even" for even numbers and "odd" for odd numbers.
+let firstNum = 1;
+let lastNum = 100;
+for ( let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + 'is an odd number');
+    } else{
+        console.log(i + 'is an even number');
+    }
+    }
+
+//2.Write a program to compute the sum of three elements of a given array of integers. The length of the array must be 3.
+let arraySum = [1, 2, 3];
+sum = 0;
+for (let i = 0; i < arraySum.length; i++){
+    sum += arraySum
+}
+console.log(sum[i]);
+
+//7.
+let firstAngle = 29;
+let secondAngle = 59;
+for (let i = 0; i < 180; i++) {
+    if (i === 90) {
+        console.log("right");
+    } else if (i < 90) {
+        console.log("acute");
+    } else if (i > 90) {
+        console.log("obtuse");
+    }
+console.log('The third angle + i');
+}
